e64fefef4b8ceda6628167f6e78fa1448eb4f2a8
Nicholas Almeida Albuquerque <e.nicholas.albuquerque@mpt.mp.br>
2024-05-08 16:05:41 -0300

    Separate parser logic to a different file (cdilla_parser.h/c)

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ if [ ! -d ./build/ ]; then
     mkdir -p ./build/
 fi;
 
-gcc $CFLAGS -o ./build/cdilla ./src/main.c ./src/utils.c ./src/cdilla_lexer.c
+gcc $CFLAGS -o ./build/cdilla ./src/main.c ./src/utils.c ./src/cdilla_lexer.c ./src/cdilla_parser.c
